fd8cc7ef89050bda27458ee0a4d77dff939bde6a
Benoit Prigent <benoit.prigent@grenoble-em.com>
2018-02-13 11:46:37 +0100

    real all

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #READ
+
+  #Read All
+  get 'tasks', to: 'tasks#index'
+  #Read one
+  get 'tasks/:id', to: 'tasks#show'
+
 end
