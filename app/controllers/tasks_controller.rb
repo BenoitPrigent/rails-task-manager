c6eed303a8a76595f9351979dc475d31ab7950e9
Benoit Prigent <benoit.prigent@grenoble-em.com>
2018-02-13 12:38:51 +0100

    create implementation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,22 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @new_task = params.require("task").permit(:title, :details)
+    @task = Task.new(@new_task)
+    @task.save
+    redirect_to tasks_path
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #READ
 
+
+  #CREATE
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  #READ
   #Read All
   get 'tasks', to: 'tasks#index'
   #Read one
-  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/:id', to: 'tasks#show', as: :show
+
+
+
+
+
 
 end
